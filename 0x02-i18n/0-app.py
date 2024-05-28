ea8650ec3c7837b5bfe2601b07c853fb2ae52e03
Eric <stephen.erich243@gmail.com>
2024-05-28 23:40:30 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""
-A Basic flask application
-"""
-from flask import Flask
-from flask import render_template
 
+"""
+Setting up a bask flask app to create a single / route 
+and an index.html template that simply outputs 'Welcome to Holberton'
+"""
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,10 +13,10 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Renders a basic html template
+   Render a basic html template
     """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
