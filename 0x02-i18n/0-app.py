36270664fcd1b45303fa5183370c7ac2c20aa788
Eric <stephen.erich243@gmail.com>
2024-05-28 23:38:23 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,18 +5,15 @@ Setting up a bask flask app to create a single / route
 and an index.html template that simply outputs 'Welcome to Holberton'
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-# Create a sigle route
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Renders a basic html template
+   Render a basic html template
     """
     return render_template('0-index.html')
 
