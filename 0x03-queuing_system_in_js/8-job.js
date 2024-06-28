2b5a829b68b3d63960a66a45d44d11ef8e800778
Eric <stephen.erich243@gmail.com>
2024-06-28 08:57:06 +0000

    queuing system in js

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,17 @@
+export function createPushNotificationsJobs(jobs, queue) {
+    if (!Array.isArray(jobs)) {
+        throw new Error('Jobs is not an array');
+    }
+    
+    for (const jobInfo of jobs) {
+        const job = queue.create('push_notification_code_3', jobInfo);
+        job
+          .on('enqueue', () => console.log('Notification job created: ', job.id))
+          .on('complete', () => console.log('Notification job '+ job.id + ' completed'))
+          .on('failed', (err) => console.log('Notification job '+ job.id + ' failed: ', err))
+          .on('progress', (progress, _data) => console.log('Notification job ' + job.id + '' + progress + '% complete'));
+        job.save();
+    }
+}
+
+export default createPushNotificationsJobs;
